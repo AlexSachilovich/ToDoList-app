2579c6da459c56a3ab6acc8ab6c2ed5e0c8e1a7e
AlexSachilovich <AlexSachilovich@gmail.com>
2022-11-15 21:25:32 +0300

    send task

--- src/components/molecules/InputGroup/InputGroup.js
+++ src/components/molecules/InputGroup/InputGroup.js
@@ -1,15 +1,47 @@
 import { Component } from "../../../core";
+import {todoList} from '../../../services/todoList/todoList'
 import '../../atoms/Button/Button'
 import '../../atoms/Input/Input'
 
 
 
 
-export class InputGroup extends Component {
+
+export class InputGroup extends Component { 
+
+    constructor() {
+        super()
+        this.state = {
+            inputValue: ""
+        }
+    }
+
+
+    onSave() {
+        if(this.state.inputValue) {
+                todoList.createTask
+        }
+    }
+    onInput(evt){
+        this.setState((state) => {
+            return {
+                ...state,
+                inputValue: evt.detail.value
+            }
+        })
+    }
+
+    componentDidMount() {
+        this.addEventListener('save-task', this.onSave)
+        this.addEventListener('custom-input', this.onInput)
+    }
+
+
+
     render(){
         return `
         <div class="input-group mb-3">
-            <my-input placeholder="Add a new task" type="text"></my-input>
+            <my-input value="${this.state.inputValue}" placeholder="Add a new task" type="text"></my-input>
             <button class="btn btn-outline-primary" type="button" id="button-addon2">save</button>
         </div>
         `
